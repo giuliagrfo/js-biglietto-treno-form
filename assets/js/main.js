2d1137c63885f7c9427f137976991b9b64111d6a
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-10 10:27:50 +0200

    fix ticket-price

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,6 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 */
 // Chiedere nome del passeggero
 const userName = document.getElementById('name').value;
-// console.log('nome utente', document.getElementById('name').value);
 
 // Chiedere numero di chilometri che vuole percorrere
 const kilometres = document.getElementById('km').value;
@@ -18,25 +17,8 @@ const kilometres = document.getElementById('km').value;
 // Chiedere l'età del passeggero
 const userAge = document.getElementById('age').value;
 
-// Calcolare il prezzo totale del viaggio
-const priceOfKilometer = 0.21;
-
-const ticketPrice = kilometres * 0.21;
-
-// Applicare uno sconto del 20% per i minorenni
-const percentageUnder18= 20;
-
-const discountValue18 = (ticketPrice / 100) * percentageUnder18; 
-
-const finalPrice18 = ticketPrice - discountValue18.toFixed(2);
 
 
-// Applicare uno sconto del 40% per gli over 65
-const percentageOver65= 40;
-
-const discountValue65 = (ticketPrice / 100) * percentageOver65; 
-
-const finalPrice65 = ticketPrice - discountValue65.toFixed(2);
 
 // Genera biglietto
 
@@ -47,13 +29,37 @@ buttonGenerator.addEventListener('click', function(){
     const userName = document.getElementById('name').value;
     // console.log(userName.value);
     document.getElementById('nome_utente').innerHTML = `${userName}`
+    
+    // Calcolare il prezzo totale del viaggio
+    const kilometres = document.getElementById('km').value;
 
+    const priceOfKilometer = 0.21;
+    
+    const ticketPrice = kilometres * priceOfKilometer;
+    
+    // Applicare uno sconto del 20% per i minorenni
+    const percentageUnder18= 20;
+    
+    const discountValue18 = (ticketPrice / 100) * percentageUnder18; 
+    
+    const finalPrice18 = ticketPrice - discountValue18.toFixed(2);
+    
+    
+    // Applicare uno sconto del 40% per gli over 65
+    const percentageOver65= 40;
+    
+    const discountValue65 = (ticketPrice / 100) * percentageOver65; 
+    
+    const finalPrice65 = ticketPrice - discountValue65.toFixed(2);
+    const userAge = document.getElementById('age').value;
+    
+    
     if (userAge < 18) {
-        document.getElementById('price').innerHTML = `${finalPrice18} $`;
+        document.getElementById('price').innerHTML = `${finalPrice18} &euro;`;
     } else if (userAge > 65) {
-        document.getElementById('price').innerHTML = `${finalPrice65} $`;
+        document.getElementById('price').innerHTML = `${finalPrice65} &euro`;
     } else {
-        document.getElementById('price').innerHTML = `${ticketPrice} $`;
+        document.getElementById('price').innerHTML = `${ticketPrice} &euro`;
     }
     
     
